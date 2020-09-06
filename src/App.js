6e111799bc521d929863edbfa2c3c6761d7bb8eb
albseb511 <albertsebastian511@gmail.com>
2020-09-06 15:05:45 +0530

    Added: test suite for app and button

--- src/App.js
+++ src/App.js
@@ -1,26 +1,36 @@
 import React from 'react';
-import logo from './logo.svg';
+import Button from "./Component/Button"
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props){
+    super(props);
+    this.state={
+      count: 0
+    }
+  }
+  handleAdd = () => {
+    this.setState({
+      count: this.state.count + 1
+    })
+  }
+  handleReduce = () => {
+    this.setState({
+      count: this.state.count - 1
+    })
+  }
+  render(){
+    const {count} = this.state
+    return (
+      <div className="App">
+          <h1>Count:</h1>
+          <h1 data-testid="counter">{count}</h1>
+
+          <Button label="ADD" onClick={this.handleAdd} />
+          <Button label="REDUCE" onClick={this.handleReduce} />
+      </div>
+    )
+  }
 }
 
 export default App;
